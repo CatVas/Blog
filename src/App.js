981e4c8a1fc4c719930ee1e3e8081fff36ad60de
Vasyl Samusiev <catvaas@googlemail.com>
2020-03-13 10:51:48 +0200

    init: Pages, layouts.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
+import { HomePage } from 'pages'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      Blog
-    </div>
-  );
+    <HomePage />
+  )
 }
 
 export default App
